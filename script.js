610b2b79e74e3f4b76bf340e969d8e025831c3b6
FelipeEsaki <120192433+FelipeEsaki@users.noreply.github.com>
2023-08-04 19:55:00 -0300

    Add files via upload
    Mobile Commit

--- script.js
+++ script.js
@@ -2,14 +2,28 @@ let sun = document.getElementById ('sun');
 let shadow = document.getElementById ('shadow');
 let samurai = document.getElementById ('samurai');
 let title = document.getElementById ('title');
+let menu = document.getElementById ('menu')
 
 window.addEventListener ('scroll', function() {
     let value = this.window.scrollY;
 
     sun.style.marginTop = value * .3 + 'px';
     shadow.style.marginLeft = value * -.3 + 'px';
-    title.style.marginLeft = value * -4 + 'px';
+    title.style.marginRight = value * 4 + 'px';
+    title.style.marginTop = value * 0.5 + 'px';
 })
+
+function clickMenu() {
+    if (menu.style.display == 'none') {
+        menu.style.display = 'flex'
+    } 
+    else {
+        menu.style.display = 'none'
+    }
+
+}
+
+
 
 let count = 1;
 document.getElementById ('radio1').checked = true;
